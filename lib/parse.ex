46d2b0843658980528b49a4c6c465db9969cbe56
Mohd Norhaimi <haimiyahya@gmail.com>
2023-07-23 01:43:14 +0800

    exclude bit 1 from the list of the parsed bit list from bitmap

--- lib/parse.ex
+++ lib/parse.ex
@@ -123,12 +123,14 @@ defmodule ElixirISO8583.Parse do
   def bmp(:ascii, msg = <<first_char::8, _rest::binary>>)
     when first_char > ?1 and first_char < ?8 do # if the first bit is not set, the message only contains first bmp
     <<bitmap::binary-size(16), rest::binary>> = msg
-    {bitmap_to_list(Base.decode16!(bitmap)), rest}
+    <<0::1, bmp::63>> = Base.decode16!(bitmap)
+    {bitmap_to_list(<<0::1, bmp::63>>), rest}
   end
 
   def bmp(:ascii, msg) do
     <<bitmap::binary-size(32), rest::binary>> = msg # the msg contains both bitmap
-    {bitmap_to_list(Base.decode16!(bitmap)), rest}
+    <<1::1, bmp::127>> = Base.decode16!(bitmap)
+    {bitmap_to_list(<<0::1, bmp::127>>), rest}
   end
 
   def bmp(:bin, <<0::1, bmp::63, rest::binary>>) do
